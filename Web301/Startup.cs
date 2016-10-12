3464ba2fa85f9fb448dfeb7b1af33f42b3eb2687
clairesligo <clairesligo@hotmail.com>
2016-10-12 15:45:32 +0100

    Add project files.

---
+++ Web301/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web301.Startup))]
+namespace Web301
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
